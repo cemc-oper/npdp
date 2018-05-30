0cca2830bf8159d81df337026347158d152e023d
perillaroc <perillaroc@gmail.com>
2018-05-30 12:33:33 +0800

    add label tag.

---
+++ npdp-web/src/components/label_tag.vue
@@ -0,0 +1,24 @@
+<template>
+  <Tag type="border" :color="label_color">
+    {{label}}
+  </Tag>
+</template>
+
+<script>
+  export default {
+    name: 'LabelTag',
+    props: [
+      'tag_color_map',
+      'label'
+    ],
+    computed: {
+      label_color: function(){
+        if (this.tag_color_map.has(this.label)){
+          return this.tag_color_map[this.label];
+        } else {
+          return 'blue';
+        }
+      }
+    }
+  }
+</script>
--- npdp-web/src/components/search_result_list.vue
+++ npdp-web/src/components/search_result_list.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <Row>
-      <Col span="20">
-        <Row v-for="(record, index) in search_results" :key="index">
+      <Col span="18" offset="2">
+        <Row v-for="(record, index) in search_results" :key="index" class="search-record">
           <Col span="24">
             <Row>
-              <Tag color="blue" v-for="(label, index) in record.labels" :key="index">
-                {{label}}
-              </Tag>
+              <h3>{{record.props.name}}</h3>
+            </Row>
+            <Row>
+              <LabelTag v-for="(label, index) in record.labels" :key="index"
+                        :tag_color_map="tagColorMap"
+                        :label="label"
+              ></LabelTag>
             </Row>
             <Row>
               {{record.props}}
@@ -20,14 +24,36 @@
 </template>
 
 <script>
+  import LabelTag from './label_tag.vue'
+
   export default {
     name: 'SearchResultList',
     props: [
       'search_results'
-    ]
+    ],
+    components: {
+      LabelTag
+    },
+    computed: {
+      tagColorMap: function(){
+        const search_results = this.search_results;
+        let color_set = new Set();
+        search_results.forEach((item, i)=>{
+          const {labels} = item;
+          labels.forEach(label=>{
+            color_set.add(label);
+          })
+        });
+        console.log(search_results);
+        return new Map();
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .search-record {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
